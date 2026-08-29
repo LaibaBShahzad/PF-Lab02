e872436571ad1d029a862643b9f0e4bbf2958072
LaibaBShahzad <laibabshahzad@gmail.com>
2026-08-29 21:42:22 +0500

    Create p260000_task2.c

---
+++ p260000_task2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int Batch = 2026;
+  char Section = 'SE-1B';
+  float Entry_test_score = 63.95;
+  double Semester_tution_fee = 268500;
+  char motto = '"Mistakes makes a man perfect"';
+
+                printf("I am from batch: \t\n%d",Batch);
+                printf("Our section is: \t\n%c",Section);
+                printf("My entry test score was: \t\n%f",Entry_test_score);
+                printf("My semester tution fee is: \t\n%lf",Semester_tution_fee);
+                printf("My moto is: \t\n%c",motto);
+
+          return 0;
+}
