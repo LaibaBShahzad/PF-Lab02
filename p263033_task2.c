039e7bc7e4a7bf28293b1dfbeb0b46bbe99f8b60
LaibaBShahzad <laibabshahzad@gmail.com>
2026-08-30 18:14:38 +0500

    p263033_task2.c

---
+++ p263033_task2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int Batch = 2026;
+  char Section = 'B';
+  float Entry_test_score = 63.95;
+  double Semester_tution_fee = 268500;
+
+                printf("Batch: \t\t\t\t\t %d",Batch);
+                printf("\nSection: \t\t\t\t %c",Section);
+                printf("\nEntry test score : \t\t %f",Entry_test_score);
+                printf("\nSemester tution fee : \t %lf",Semester_tution_fee);
+                printf("\n Motto: \t\t\t\t \"Mistakes makes a man perfect\"");
+
+          return 0;
+}
